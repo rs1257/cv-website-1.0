bf065015c69e66cabfd88998a4a185c7a8fe7b70
Ryan Smith <ryansmith300082@gmail.com>
2024-06-26 13:58:04 +0100

    feat: initial commit seeting up project and starting on footer and colour palette

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import styles from "./footer.module.scss";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <b>Ryan Smith</b>
+        <div>&copy; {currentYear} All rights reserved.</div>
+      </div>
+      <div className={styles.iconContainer}>
+        <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+        <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+      </div>
+    </footer>
+  );
+}
