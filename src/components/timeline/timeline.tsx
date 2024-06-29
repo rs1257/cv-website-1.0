75d3c9ab84ae2c92af59cbf159270fdc6dd47789
Ryan Smith <ryansmith300082@gmail.com>
2024-06-29 15:46:24 +0100

    feat: interactive timeline components

--- src/components/timeline/timeline.tsx
+++ src/components/timeline/timeline.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Timeline from "@mui/lab/Timeline";
 import TimelineItem from "@mui/lab/TimelineItem";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
@@ -9,6 +10,8 @@ import Chip from "@mui/material/Chip";
 import { EXPERIENCE } from "@/config/globals";
 import Section from "../section/section";
 import { babyBlue, lightBlue } from "@/config/colours";
+import { useState } from "react";
+import { domAnimation, LazyMotion, m } from "framer-motion";
 
 export default function OutlinedTimeline() {
   const data = [
@@ -50,50 +53,67 @@ export default function OutlinedTimeline() {
     },
   ];
 
+  const [selected, setSelected] = useState<number>(0);
+
   return (
     <Section
       id={EXPERIENCE}
       header="Experience"
       text="My software engineer journey"
     >
-      <Timeline position="alternate-reverse" className={styles.timeline}>
-        {data.map(({ name, startDate, endDate, body }, index) => {
-          return (
-            <TimelineItem key={name}>
-              <TimelineSeparator>
-                <TimelineDot
-                  variant="outlined"
-                  sx={{ borderColor: babyBlue }}
-                />
-                {data.length - 1 !== index && (
-                  <TimelineConnector sx={{ backgroundColor: lightBlue }} />
-                )}
-              </TimelineSeparator>
-              <TimelineContent>
-                <div
-                  className={`${styles.card} ${
-                    index % 2 === 1 ? styles.left : styles.right
-                  }`}
-                >
-                  <h3>{name}</h3>
-                  <span className={styles.dateRange}>
-                    {startDate} - {endDate}
-                  </span>
-                  <div>
-                    <Chip
-                      label="Small"
-                      size="small"
-                      color="info"
-                      sx={{ background: "red" }}
+      <LazyMotion features={domAnimation} strict>
+        <Timeline position="alternate-reverse" className={styles.timeline}>
+          {data.map(({ name, startDate, endDate, body }, index) => {
+            return (
+              <TimelineItem key={name}>
+                <TimelineSeparator>
+                  <TimelineDot
+                    variant={selected === index ? "filled" : "outlined"}
+                    sx={{
+                      borderColor: babyBlue,
+                      background: selected === index ? babyBlue : "none",
+                      width: "10px",
+                      height: "10px",
+                    }}
+                  />
+                  {data.length - 1 !== index && (
+                    <TimelineConnector
+                      sx={{
+                        backgroundColor: lightBlue,
+                        width: "5px",
+                        borderRadius: "10px",
+                      }}
                     />
-                  </div>
-                  <div>{body}</div>
-                </div>
-              </TimelineContent>
-            </TimelineItem>
-          );
-        })}
-      </Timeline>
+                  )}
+                </TimelineSeparator>
+                <TimelineContent>
+                  <m.div
+                    whileHover={{ scale: 1.02 }}
+                    className={`${styles.card} ${
+                      index % 2 === 1 ? styles.left : styles.right
+                    }`}
+                    onMouseOver={() => setSelected(index)}
+                  >
+                    <h3>{name}</h3>
+                    <span className={styles.dateRange}>
+                      {startDate} - {endDate}
+                    </span>
+                    <div>
+                      <Chip
+                        label="Small"
+                        size="small"
+                        color="info"
+                        sx={{ background: "red" }}
+                      />
+                    </div>
+                    <div>{body}</div>
+                  </m.div>
+                </TimelineContent>
+              </TimelineItem>
+            );
+          })}
+        </Timeline>
+      </LazyMotion>
     </Section>
   );
 }
